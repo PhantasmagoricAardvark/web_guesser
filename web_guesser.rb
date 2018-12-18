88eb38604b2ac5fe567bcc5ac46ca8ac751ca4db
seba <villeaho@tutanota.com>
2018-12-18 16:48:11 +0200

    The background color changes depending on the message.

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,34 +1,48 @@
 require "sinatra"
 require "sinatra/reloader"
+require "pry"
 
-
-message = ""
 
 def check_guess(guess,num) 
-
 	if (guess).to_i > (num + 5)
-		message = "Way too high!"
+		return "Way too high!"
 	elsif (guess).to_i < (num - 5)
-		message = "Way too low!"
+		return "Way too low!"
 	elsif (guess).to_i > num	
-		message = "Too high!"
-	elsif (guess).to_i < num	
-		message = "Too low!"
+		return "Too high!"
+	elsif (guess).to_i < num			
+		return "Too low!"
 	else
-		message = "You got it right!"
+		return "You got it right!"
 	end	
 end
 
-num = rand(0..100) 
+def change_color(message) 
 
+	if message == "Way too high!" || message == "Way too low!"
+		return "red"
+	elsif message == "Too high!" || message == "Too low!"
+		return "#ffcccc"
+	elsif message == "You got it right!"
+		return "green"	
+	end
+		
+end
+
+num = rand(0..100) 
+message = ""
+$color = ""
 
 get "/" do
+	color = ""
 
 	guess = (params["guess"].to_i)
 	message	= check_guess(guess,num)
+	$color = change_color(message)
+	color = $color
 
-
-	erb :index, :locals => {:num => num, :message => message }
+	erb :index, :locals => {:num => num, :message => message, :color => color }
 
 end
+
 
